ea48468370d06cad6d77fac2017a4be9850e18b8
Matthew Richardson <matthew_richardson@ymail.com>
2024-07-24 03:54:23 +0100

    computer choice math.floor math.random test

---
+++ script.js
@@ -0,0 +1,10 @@
+choiceArray["Rock", "Paper", "Scissors"]
+
+function getComputerChoice(){
+    console.log("test")
+    return Math.floor(math.random()*choiceArray)
+}
+
+getComputerChoice()
+
+console.log(getComputerChoice());
