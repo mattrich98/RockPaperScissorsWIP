5a18800e957d67f45aae1875f5e9e1ae81bf7bec
Matthew Richardson <matthew_richardson@ymail.com>
2024-07-24 04:15:16 +0100

    added some explanation comments to getComputerChoice Function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getComputerChoice(){
     //console.log("Computer Rolls:")
     let computerChoiceArray = ["Rock", "Paper", "Scissors"]
-    randomChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)]
+    randomChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)] // maths.floor makes it a whole number -> maths.random * array length 0,1,2 (uses the array)
     //console.log(randomChoice)
-    return 
+    return //want to return the result of random choice whenever uses function
 }
 
 getComputerChoice()
