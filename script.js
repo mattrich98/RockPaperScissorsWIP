0341d79f1963e635543595f8548b604c1b071c82
Matthew Richardson <matthew_richardson@ymail.com>
2024-07-24 04:21:13 +0100

    figured out how to use return within a function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function getComputerChoice(){
     //console.log("Computer Rolls:")
     let computerChoiceArray = ["Rock", "Paper", "Scissors"]
-    randomChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)] // maths.floor makes it a whole number -> maths.random * array length 0,1,2 (uses the array)
+    randomChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)]
     //console.log(randomChoice)
-    return //want to return the result of random choice whenever uses function
+    return randomChoice //returns the results to the used function
 }
 
-getComputerChoice()
+console.log(getComputerChoice()); //console.log is worded like this to see the result of using the function
+console.log(getComputerChoice());
+console.log(getComputerChoice());
