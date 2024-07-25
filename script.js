3e1a462890ee163c5d9b84896fffc28178f446e2
Matthew Richardson <matthew_richardson@ymail.com>
2024-07-25 02:27:12 +0100

    renamed randomChoice to computerChoice variable, started playRound function with parameters

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let computerScore = 0;
 function getComputerChoice(){
     //console.log("Computer Rolls:")
     let computerChoiceArray = ["Rock", "Paper", "Scissors"]
-    randomChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)] //math.floor() makes it a whole number, math.random() randomises multiplied by array lenght within computerChoiceArray variable array brackets [].
+    computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)] //math.floor() makes it a whole number, math.random() randomises multiplied by array lenght within computerChoiceArray variable array brackets [].
     //console.log(randomChoice)
-    return randomChoice //returns the results to the used function
+    return computerChoice //returns the results to the used function
 }
-/*
+/* testing
 console.log(getComputerChoice()); //console.log is worded like this to see the result of using the function
 console.log(getComputerChoice());
 console.log(getComputerChoice());
@@ -19,4 +19,8 @@ function getHumanChoice(){
     return humanChoice //To put the result in a used function
 }
 
-console.log(getHumanChoice());
+//console.log(getHumanChoice()); testing
+
+function playRound(humanChoice, computerChoice){
+
+}
