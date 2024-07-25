ad7653db850acc8a075f569c7dd4e0341145c467
Matthew Richardson <matthew_richardson@ymail.com>
2024-07-25 02:33:03 +0100

    added .toLowerCase() method to end of() prompt method

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let computerScore = 0;
 
 function getComputerChoice(){
     //console.log("Computer Rolls:")
-    let computerChoiceArray = ["Rock", "Paper", "Scissors"]
+    let computerChoiceArray = ["rock", "paper", "scissors"]
     computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)] //math.floor() makes it a whole number, math.random() randomises multiplied by array lenght within computerChoiceArray variable array brackets [].
     //console.log(randomChoice)
     return computerChoice //returns the results to the used function
@@ -15,12 +15,12 @@ console.log(getComputerChoice());
 */
 
 function getHumanChoice(){
-    let humanChoice = prompt("Rock, Paper, Scissors?") //Opens a window where you write your answer and press ok
+    let humanChoice = prompt("Rock, Paper, Scissors?").toLowerCase() //Opens a window where you write your answer and press ok
     return humanChoice //To put the result in a used function
 }
 
 //console.log(getHumanChoice()); testing
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice){ //parameters go inside brackets ()
 
 }
