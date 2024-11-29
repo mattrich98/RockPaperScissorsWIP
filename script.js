085a1be10b76bacb5f49d883d856e6826cf76e36
Matthew Richardson <matthew_richardson@ymail.com>
2024-11-29 07:23:03 +0000

    javascript didnt work on HTML because I didn't link to HTML

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(){
-    //console.log("Computer Rolls:")
+    console.log("Computer Rolls:")
     let computerChoiceArray = ["rock", "paper", "scissors"]
     computerChoice = computerChoiceArray[Math.floor(Math.random()*computerChoiceArray.length)] //math.floor() makes it a whole number, math.random() randomises multiplied by array length (its different from string.length) within computerChoiceArray variable array brackets [].
-    //console.log(computerChoice)
+    console.log(computerChoice)
     return computerChoice //returns the results to the used function
 }
-/* testing
+
 console.log(getComputerChoice()); //console.log is worded like this to see the result of using the function
 console.log(getComputerChoice()); //to test if it does other results
 console.log(getComputerChoice()); //to test if it does other results 2
-*/
+
 
 function getHumanChoice(){
     let humanChoice = prompt("rock, paper, scissors?").toLowerCase() //Opens a window where you write your answer and press ok. + added .lowerCase() to end of prompt to make case insensitive
@@ -26,3 +26,5 @@ function playRound(humanChoice, computerChoice){ //parameters go inside brackets
 
 
 }
+
+console.log("test");
